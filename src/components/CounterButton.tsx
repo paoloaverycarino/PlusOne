293c0e71a9dfd8dfe12386d9e56d68c2ad8b1f78
Kosue3 <Andyt21202@gmail.com>
2025-01-11 11:26:48 -0600

    Counter Button

---
+++ src/components/CounterButton.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../services/firebase";
+
+const CounterButton: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+
+  // Fetch counter value from Firestore when the component mounts
+  useEffect(() => {
+    getUserCounter();
+  }, []);
+
+  // Get counter value from Firestore
+  const getUserCounter = async () => {
+    const counterRef = doc(db, "counters", "user1"); // Reference to user-specific document
+    const docSnap = await getDoc(counterRef);
+
+    if (docSnap.exists()) {
+      setCounter(docSnap.data().counter); // Set the counter state with the value from Firestore
+    } else {
+      await setDoc(counterRef, { counter: 0 }); // If document doesn't exist, create it with an initial value
+      setCounter(0); // Initialize local state
+    }
+  };
+
+  // Increment the counter in Firestore
+  const handleIncrementCounter = async () => {
+    const counterRef = doc(db, "counters", "user1"); // Reference to user-specific document
+    await setDoc(counterRef, { counter: counter + 1 }); // Update Firestore with the new counter value
+    setCounter(counter + 1); // Update local state to reflect new counter
+  };
+  return (
+    <>
+        <button className="btn" onClick={handleIncrementCounter}>Hello</button>
+    </>
+  );
+}
+
+export default CounterButton;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,19 @@
-
-import React from 'react';
+import React from "react";
 
 const Hero: React.FC = () => {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-
-
-        <div className="card bg-gradient-to-r from-blue-500 to-purple-600 w-96 shadow-xl">
-
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                    </div>
-            </div>
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="card bg-gradient-to-r from-blue-500 to-purple-600 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
       </div>
-    
+    </div>
+  );
+};
 
-    );
-  };
-  
-  export default Hero;
-
+export default Hero;
