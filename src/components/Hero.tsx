139fde49cf43f1e263e6d3f4341591f603ea88d8
Paolo Avery Carino <paoloaverycarino@gmail.com>
2025-01-11 12:25:58 -0500

    Login

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,10 +10,15 @@ const Hero: React.FC = () => {
             <div className="card bg-gradient-to-b from-[#FF6422] to-[#99147C] w-96 shadow-xl border-4 border-whi">
 
                 <div className="card-body">
-                    <h1 className="font-neue text-4xl text-white">PlusOne</h1>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h1 className="font-neue font-bold text-4xl text-white">PlusOne</h1>
+                    <h3 className="font-neue font-light text-sm text-white">Username</h3>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <h3 className="font-neue font-light text-sm text-white">Password</h3>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        
+                        
                         <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-sm btn-primary">Submit</button>
                         </div>
                 </div>
             </div>
