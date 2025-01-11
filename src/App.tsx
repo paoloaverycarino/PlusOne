3cc11ced114f62738106954eff44c5fa984e5bb1
Kosue3 <Andyt21202@gmail.com>
2025-01-11 13:53:06 -0600

    buttons work for both users

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Hero from './pages/Hero';
-import User from './pages/User';
-import './index.css';
+import React from "react";
+import { UserProvider } from "./contexts/UserContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hero from "./pages/Hero";
+import User from "./pages/User";
+import "./index.css";
 
 const App: React.FC = () => {
   return (
-
+    <UserProvider>
       <Router basename="/PlusOne/">
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/User" element={<User />} />
         </Routes>
       </Router>
+    </UserProvider>
   );
 };
 
-export default App
+export default App;
