c0102106354e007cbba8461278b99a2c521ef405
Paolo Avery Carino <paoloaverycarino@gmail.com>
2025-01-11 14:56:58 -0500

    changes?

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -4,7 +4,7 @@ import CounterButton from "../components/CounterButton";
 const User: React.FC = () => {
     return (
        <body className="flex items-center justify-center min-h-screen bg-white] ">
-        <div className="card glass bg-gradient-to-b from-[#A0E5EA] to-[#84DB6E] w-96 shadow-xl">
+        <div className="card glass bg-gradient-to-b from-[#A0E5EA] to-[#84DB6E] w-80 shadow-xl">
             <div className="card-body items-center text-center">
                 <h1 className="card-title font-neue font-bold text-7xl text-white">Total</h1>
                 <h1 className="card-title font-neue font-bold text-9xl text-white">0</h1>
