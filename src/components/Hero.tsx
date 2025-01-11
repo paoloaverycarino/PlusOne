23b33a55c12c4d6fb617c921db084ccb604c353c
Paolo Avery Carino <paoloaverycarino@gmail.com>
2025-01-11 12:11:33 -0500

    Neue Montreal Font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,21 +3,22 @@ import React from 'react';
 
 const Hero: React.FC = () => {
     return (
-      <div className="flex items-center justify-center min-h-screen">
+       <body className="bg-[#525252]">
+        <div className="flex items-center justify-center min-h-screen">
 
 
-        <div className="card bg-gradient-to-r from-blue-500 to-purple-600 w-96 shadow-xl">
+            <div className="card bg-gradient-to-b from-[#FF6422] to-[#99147C] w-96 shadow-xl border-4 border-whi">
 
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                    </div>
+                <div className="card-body">
+                    <h1 className="font-neue text-4xl text-white">PlusOne</h1>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                </div>
             </div>
         </div>
-      </div>
-    
+      </body>
 
     );
   };
