eaacc4316208229727855d490dff3c433d70befd
Paolo Avery Carino <paoloaverycarino@gmail.com>
2025-01-12 15:38:16 -0500

    Updates

--- src/components/LoadingScreen.tsx
+++
@@ -1,9 +0,0 @@
-function LoadingSpinner() {
-  return (
-    <div>
-      <span className="loading loading-infinity loading-lg text-sky-400"></span>
-    </div>
-  );
-}
-
-export default LoadingSpinner;
