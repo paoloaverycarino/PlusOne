77e9765c4dce6835979e8a0f800203471292f077
Paolo Avery Carino <paoloaverycarino@gmail.com>
2025-01-11 20:16:44 -0500

    what

--- src/components/ContributionGraph.tsx
+++ src/components/ContributionGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const ContributionGraph: React.FC = () => {
     const [currentDate, setCurrentDate] = useState<Date>(new Date());
