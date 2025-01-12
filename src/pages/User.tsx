f6d85bcbad66c52bdd2c30b9ef3a6750c0246705
Kosue3 <Andyt21202@gmail.com>
2025-01-12 08:21:29 -0600

    button

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import CounterButton from "../components/CounterButton";
 import ContributionGraph from "../components/ContributionGraph";
 import UploadToday from "../components/UploadToday";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { db } from "../services/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { useUser } from "../contexts/UserContext";
@@ -19,19 +19,19 @@ const User: React.FC = () => {
   }, [username]);
 
   const getUserCounter = async () => {
-      const user1Ref = doc(db, "counters", "user1"); // Use the username as the document ID
-      const user2Ref = doc(db, "counters", "user2"); // Use the username as the document ID
+    const user1Ref = doc(db, "counters", "user1"); // Use the username as the document ID
+    const user2Ref = doc(db, "counters", "user2"); // Use the username as the document ID
 
-      const user1Snap = await getDoc(user1Ref);
-      const user2Snap = await getDoc(user2Ref);
+    const user1Snap = await getDoc(user1Ref);
+    const user2Snap = await getDoc(user2Ref);
 
-      if (user1Snap.exists()) {
-        setUser1Counter(user1Snap.data().counter || 0); // Set the counter state with the value from Firestore
-      }
+    if (user1Snap.exists()) {
+      setUser1Counter(user1Snap.data().counter || 0); // Set the counter state with the value from Firestore
+    }
 
-      if (user2Snap.exists()) {
-        setUser2Counter(user2Snap.data().counter || 0); // Set the counter state with the value from Firestore
-      }
+    if (user2Snap.exists()) {
+      setUser2Counter(user2Snap.data().counter || 0); // Set the counter state with the value from Firestore
+    }
   };
 
   const handleUser1CounterUpdate = (newCounter: number) => {
@@ -42,17 +42,18 @@ const User: React.FC = () => {
     setUser2Counter(newCounter); // Update the local state in User component when the counter changes
   };
 
-const isUser1 = username === "user1";
+  const isUser1 = username === "user1";
 
   return (
-
     <body className="flex flex-col items-center justify-center min-h-screen bg-white">
       <div className="h-[90vh] flex items-center justify-center">
         <div className="flex gap-6 mb-4">
           {/* Card for Upload Today */}
           <div className="card bg-black w-[32rem] h-[32rem] shadow-2xl">
             <div className="card-body flex flex-col items-center space-y-4 text-center">
-              <h1 className="card-title font-neue font-bold text-8xl text-transparent bg-clip-text bg-gradient-to-r from-[#FED90B] to-[#FF8C00] leading-[1.2]">Progress</h1>
+              <h1 className="card-title font-neue font-bold text-8xl text-transparent bg-clip-text bg-gradient-to-r from-[#FED90B] to-[#FF8C00] leading-[1.2]">
+                Progress
+              </h1>
               <div className="grid grid-cols-2 grid-rows-2 gap-6 w-full h-full">
                 <div className="stats shadow col-span-1 row-span-2">
                   <div className="card bg-white">
@@ -60,13 +61,23 @@ const isUser1 = username === "user1";
                   </div>
                 </div>
                 <div className="stat flex items-center justify-center rounded-md bg-white transform transition-all duration-300 ease-in-out hover:scale-105">
-                  <Link to="/calendar" className="w-full h-full flex items-center justify-center">
-                    <div className="stat-title font-neue font-bold text-black">Calendar Gallery</div>
+                  <Link
+                    to="/calendar"
+                    className="w-full h-full flex items-center justify-center"
+                  >
+                    <div className="stat-title font-neue font-bold text-black">
+                      Calendar Gallery
+                    </div>
                   </Link>
                 </div>
                 <div className="stat flex items-center justify-center rounded-md bg-white transform transition-all duration-300 ease-in-out hover:scale-105">
-                  <Link to="/training" className="w-full h-full flex items-center justify-center">
-                    <div className="stat-title font-neue font-bold text-black">Training Regime</div>
+                  <Link
+                    to="/training"
+                    className="w-full h-full flex items-center justify-center"
+                  >
+                    <div className="stat-title font-neue font-bold text-black">
+                      Training Regime
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -79,19 +90,32 @@ const isUser1 = username === "user1";
               {/* User 1 Counter on the Left */}
               <div className="card glass bg-gradient-to-b from-[#7691CB] to-[#45FFF0] w-80 shadow-2xl">
                 <div className="card-body items-center flex-col justify-between h-full text-center">
-                  <h1 className="card-title font-neue font-bold text-8xl text-white">Total</h1>
-                  <h1 className="card-title font-neue font-bold text-[10rem] text-white">{user1Counter}</h1>
+                  <h1 className="card-title font-neue font-bold text-8xl text-white">
+                    Total
+                  </h1>
+                  <h1 className="card-title font-neue font-bold text-[10rem] text-white">
+                    {user1Counter}
+                  </h1>
                   <div className="card-actions">
-                    <CounterButton currentCounter={user1Counter} onCounterUpdate={handleUser1CounterUpdate}/>
+                    <CounterButton
+                      currentCounter={user1Counter}
+                      onCounterUpdate={handleUser1CounterUpdate}
+                    />
                   </div>
                 </div>
               </div>
               <div className="card glass bg-gradient-to-b from-[#ED373A] to-[#6929A1] w-80 shadow-2xl">
                 <div className="card-body items-center flex-col justify-between h-full text-center">
-                  <h1 className="card-title font-neue font-bold text-8xl text-white">Total</h1>
-                  <h1 className="card-title font-neue font-bold text-[10rem] text-white pb-10">{user2Counter}</h1>
+                  <h1 className="card-title font-neue font-bold text-8xl text-white">
+                    Total
+                  </h1>
+                  <h1 className="card-title font-neue font-bold text-[10rem] text-white">
+                    {user2Counter}
+                  </h1>
                   <div className="card-actions">
-                  <button className="font-neue btn btn-wide bg-white bg-opacity-20 backdrop-blur-lg text-white font-bold py-2 px-6 rounded-lg shadow-lg border-2 border-white hover:bg-opacity-40 hover:scale-105 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 opacity-1">+1</button>
+                    <button className="font-neue btn btn-wide bg-white bg-opacity-20 backdrop-blur-lg text-white font-bold py-2 px-6 rounded-lg shadow-lg border-2 border-white hover:bg-opacity-40 hover:scale-105 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 opacity-1">
+                      +1
+                    </button>
                   </div>
                 </div>
               </div>
@@ -101,19 +125,32 @@ const isUser1 = username === "user1";
               {/* User 2 Counter on the Left */}
               <div className="card glass bg-gradient-to-b from-[#ED373A] to-[#6929A1] w-80 shadow-2xl">
                 <div className="card-body items-center flex-col justify-between h-full text-center">
-                  <h1 className="card-title font-neue font-bold text-8xl text-white">Total</h1>
-                  <h1 className="card-title font-neue font-bold text-[10rem] text-white">{user2Counter}</h1>
+                  <h1 className="card-title font-neue font-bold text-8xl text-white">
+                    Total
+                  </h1>
+                  <h1 className="card-title font-neue font-bold text-[10rem] text-white">
+                    {user2Counter}
+                  </h1>
                   <div className="card-actions">
-                    <CounterButton currentCounter={user2Counter} onCounterUpdate={handleUser2CounterUpdate}/>
+                    <CounterButton
+                      currentCounter={user2Counter}
+                      onCounterUpdate={handleUser2CounterUpdate}
+                    />
                   </div>
                 </div>
               </div>
               <div className="card glass bg-gradient-to-b from-[#7691CB] to-[#45FFF0] w-80 shadow-2xl ">
                 <div className="card-body items-center flex-col justify-between h-full text-center">
-                  <h1 className="card-title font-neue font-bold text-8xl text-white">Total</h1>
-                  <h1 className="card-title font-neue font-bold text-[10rem] text-white ">{user1Counter}</h1>
+                  <h1 className="card-title font-neue font-bold text-8xl text-white">
+                    Total
+                  </h1>
+                  <h1 className="card-title font-neue font-bold text-[10rem] text-white ">
+                    {user1Counter}
+                  </h1>
                   <div className="card-actions">
-                    <button className="font-neue btn btn-wide bg-white bg-opacity-20 backdrop-blur-lg text-white font-bold py-2 px-6 rounded-lg shadow-lg border-2 border-white hover:bg-opacity-40 hover:scale-105 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 opacity-1">+1</button>
+                    <button className="font-neue btn btn-wide bg-white bg-opacity-20 backdrop-blur-lg text-white font-bold py-2 px-6 rounded-lg shadow-lg border-2 border-white hover:bg-opacity-40 hover:scale-105 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 opacity-1">
+                      +1
+                    </button>
                   </div>
                 </div>
               </div>
